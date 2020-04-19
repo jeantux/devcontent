594bb49d9f26d1f6fa9621393f41bd8009fcc335
Jeaanca <jeaanca.santos@gmail.com>
2020-04-19 00:22:44 -0300

    added google analytics

--- app/src/main.js
+++ app/src/main.js
@@ -4,6 +4,11 @@ import router from './router'
 import './../node_modules/bulma/css/bulma.css';
 import 'font-awesome/css/font-awesome.css';
 import './assets/css/index.css'
+import VueAnalytics from 'vue-analytics'
+
+Vue.use(VueAnalytics, {
+  id: 'UA-163993851-1'
+})
 
 Vue.config.productionTip = true
 
